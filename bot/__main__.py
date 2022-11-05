9e90047243007b05988880a8ddad2c49011920e7
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2022-11-05 06:26:57 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -40,11 +40,12 @@ from bot.plugins.status_message_fn import (
 from bot.commands import Command
 from bot.plugins.call_back_button_handler import button
 sudo_users = "1666551439" 
-crf.append("30")
+sudo_users = "1482769753" 
+crf.append("28")
 codec.append("libx265")
 resolution.append("1280x720")
-preset.append("ultrafast")
-audio_b.append("96k")
+preset.append("veryfast")
+audio_b.append("48k")
 # 🤣
 
 
