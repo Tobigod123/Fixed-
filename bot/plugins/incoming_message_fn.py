8d9100b39320ae9c013ef8c3a04370de407ebe6f
nikhilbakugo9864 <145025633+nikhilbakugo9864@users.noreply.github.com>
2023-09-23 17:46:50 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -180,7 +180,6 @@ async def incoming_compress_message_f(update):
         thumbnailed = "thumb.jpg"
     else:
         thumbnailed = thumb_image_path
-    width, height = get_width_height(saved_file_path)
     chat_id = LOG_CHANNEL
     utc_now = datetime.datetime.utcnow()
     ist_now = utc_now + datetime.timedelta(minutes=30, hours=5)
