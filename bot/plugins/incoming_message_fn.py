f6e775a0c887a7c1ef9cf5584c0aa4f15f05acc8
senku270 <145897244+senku270@users.noreply.github.com>
2023-09-24 20:36:45 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -112,7 +112,6 @@ async def incoming_compress_message_f(update):
       new_name = anitopy.parse(nam)
       anime_name = new_name['anime_title']
       episode_no = new_name['episode_number']  
-      resolution = new_name['file_resolution']
       joined_string = joined_string = f"S1E{episode_no} {anime_name} [{resolution}] [@Anime_Sensei_Network.mkv]"
       if 'anime_season' in new_name.keys():
         animes_season = new_name['anime_season']
