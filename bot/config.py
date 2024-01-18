f13d76eeb0ea4805f3bfd19e1ab4203e286710dd
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2024-01-18 15:41:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,16 +7,16 @@ class Config(object):
     # sucks Dude
     APP_ID = 4018758  # Updated with your API ID
     API_HASH = "622bba3cf046315531f71f9d97fa6c2a"  # Updated with your API HASH
-    LOG_CHANNEL = "ygsrhggd" # Updated with your log channel ID
+    LOG_CHANNEL = "-1002108819224" # Updated with your log channel ID
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
     # Get these values from my.telegram.org
-    AUTH_USERS = {5385471287, -1001533601450, 6445948745}
+    AUTH_USERS = {6748415360}
     # auth users jdk 
-    TG_BOT_TOKEN = "5822396703:AAG7sX8J2gaYpF9J_ZHGphpqZ4VXSdpF-P8"  # Updated with your bot token
+    TG_BOT_TOKEN = "6650630304:AAFqJ_xw_SEMydEYJJbWOmi3f89hdLJMgZ0"  # Updated with your bot token
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = "Feelpainroobot"  # Updated with your bot username
+    BOT_USERNAME = "Bot144255262626_bot"  # Updated with your bot username
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
