1e293c7a0fd3837c12adc5e6bf0159b0e5072edc
GokuOp7 <153523489+GokuOp7@users.noreply.github.com>
2024-01-18 09:06:46 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -231,7 +231,7 @@ async def incoming_compress_message_f(update):
         force_document=True,
         file_name=joined_string,
         thumb=thumbnailed,
-        reply_to_message_id=update.message_id,
+        reply_to_message_id=update.id,
         progress=progress_for_pyrogram,
         progress_args=(
           bot,
@@ -333,5 +333,5 @@ async def incoming_cancel_message_f(bot, update):
     await bot.send_message(
       chat_id=update.chat.id,
       text="No active compression exists",
-      reply_to_message_id=update.message_id
+      reply_to_message_id=update.id
     )
